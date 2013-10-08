9cc8d078ffb85ef6edc031dc1c0b5b4203b9fb9c
juno5460 <lyuno@vip.qq.com>
2013-10-08 23:57:26 +0800

    add handlebars
    add handlebars

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 exports.index = function(req, res) {
-	res.send('some html');
-	// res.render('index/index', {
-	// 	title: 'Login',
-	// 	message: "req.flash('error')"
-	// });
+	// res.send('some html');
+	res.render('index/index', {
+		title: 'Login',
+		message: "req.flash('error')"
+	});
 };
