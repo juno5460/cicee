5be295ad5bf0f8e2bf07da389cd84d3df391870f
juno5460 <lyuno@vip.qq.com>
2013-10-19 15:20:56 +0800

    update port
    update port

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ require('./config/setting')(app, config);
 require('./config/routes')(app);
 
 // Start the app by listening on <port>
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 app.listen(port);
 console.log('Express app started on port ' + port);
 
